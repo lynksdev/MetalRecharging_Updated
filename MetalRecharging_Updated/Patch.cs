324f439b36064128c615a6aba79eaae07f9d594a
lynksdev <tatatomfilms2@gmail.com>
2025-03-07 13:38:29 -0800

    Updated Metal Recharging
    Added files

---
+++ MetalRecharging_Updated/Patch.cs
@@ -0,0 +1,66 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace MetalRecharging_Updated
+{
+    [HarmonyPatch(typeof(ItemCharger))]
+    internal class ItemChargerPatch : NetworkBehaviour
+    {
+
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static bool ItemChargerUpdate(ItemCharger __instance, ref float ___updateInterval, ref InteractTrigger ___triggerScript)
+        {
+            if (NetworkManager.Singleton == null) return false;
+            if (___updateInterval > 1f)
+            {
+                ___updateInterval = 0;
+                if (GameNetworkManager.Instance != null && GameNetworkManager.Instance.localPlayerController != null)
+                {
+                    var heldObject = GameNetworkManager.Instance.localPlayerController.currentlyHeldObjectServer;
+                    if (heldObject == null || (!heldObject.itemProperties.isConductiveMetal && !heldObject.itemProperties.requiresBattery))
+                    {
+                        ___triggerScript.interactable = false;
+                        ___triggerScript.disabledHoverTip = "(Requires battery-powered item)";
+                    }
+                    else
+                    {
+                        ___triggerScript.interactable = true;
+                        // ___triggerScript.hoverTip = "Charge item : [LMB]";
+                    }
+                    ___triggerScript.twoHandedItemAllowed = true; // uuuuuuh this might break things idk. maybe do this in START or something
+                    return false;
+                }
+            }
+            ___updateInterval += Time.deltaTime;
+            return false;
+        }
+
+        [HarmonyPatch("ChargeItem")]
+        [HarmonyPostfix]
+        static void ItemChargerCharge(ItemCharger __instance)
+        {
+            var heldObject = GameNetworkManager.Instance.localPlayerController.currentlyHeldObjectServer;
+            if (heldObject != null && !heldObject.itemProperties.requiresBattery && heldObject.itemProperties.isConductiveMetal)
+            {
+                var localPlayer = GameNetworkManager.Instance.localPlayerController;
+                //Landmine.SpawnExplosion(parent.position, effect, killrange, damagerange, 50, 0f, (GameObject)null, false);
+                ((MonoBehaviour)__instance).StartCoroutine(KaboomCoroutine(localPlayer.transform.position));
+                //LandminePatch.LastExplosionWasLocalPlayer = true;
+                //ChatPatch.SendExplosionChat();
+            }
+        }
+        private static IEnumerator KaboomCoroutine(Vector3 boomLocation)
+        {
+            yield return new WaitForSeconds(0.75f);
+            Landmine.SpawnExplosion(boomLocation, true, 1f, 1f, 50, 0f, (GameObject)null, false);
+        }
+    }
+}
---
+++ MetalRecharging_Updated/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace MetalRecharging_Updated
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class MRUPlugin : BaseUnityPlugin
+    {
+        private const string modGUID = "lynksdev.MetalRecharging_Updated";
+        private const string modName = "Metal Recharing Updated";
+        private const string modVersion = "1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static MRUPlugin instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo($"Plugin {modGUID} is loaded!\n\n\n\n CHARGE YOUR METAL");
+
+            harmony.PatchAll(typeof(MRUPlugin));
+            harmony.PatchAll(typeof(ItemChargerPatch));
+        }
+    }
+}
